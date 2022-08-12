85468d0bf7f2b78a96dfd5f4df05e57ba90af85b
martin-ngigi <martinwainaina001@gmail.com>
2022-08-12 10:28:29 +0300

    Added Links

---
+++ app/src/main/java/com/example/facerecognition/Links.java
@@ -0,0 +1,8 @@
+package com.example.facerecognition;
+
+public class Links {
+    /**
+     * https://github.com/estebanuri/face_recognition
+     * https://medium.com/@estebanuri/real-time-face-recognition-with-android-tensorflow-lite-14e9c6cc53a5
+     * **/
+}
